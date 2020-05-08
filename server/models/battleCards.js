854e3443d66662307e85f2f91eca4a43fc24d10d
Maxime Chemenda <maxime.chemenda@gmail.com>
2020-05-08 20:28:37 +0200

    Mongoose Schema added

---
+++ server/models/battleCards.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+
+const battleCardsSchema = new mongoose.Schema({
+  battleCards: {
+      objectionsBattleCards: {
+          cards: {
+              cardId: String,
+              text: String,
+              cardType: String,
+              editing: Boolean
+          },
+          battleCardId: String,
+          isEmptyAddButtonState: Boolean,
+          titleValue: String,
+          section: String
+      },
+      competitorsBattleCards: {
+        cards: {
+            cardId: String,
+            text: String,
+            cardType: String,
+            editing: Boolean
+        },
+        battleCardId: String,
+        isEmptyAddButtonState: Boolean,
+        titleValue: String,
+        section: String
+    },
+    profilesBattleCards: {
+        cards: {
+            cardId: String,
+            text: String,
+            cardType: String,
+            editing: Boolean
+        },
+        battleCardId: String,
+        isEmptyAddButtonState: Boolean,
+        titleValue: String,
+        section: String
+    },
+    discoveriesBattleCards: {
+        cards: {
+            cardId: String,
+            text: String,
+            cardType: String,
+            editing: Boolean
+        },
+        battleCardId: String,
+        isEmptyAddButtonState: Boolean,
+        titleValue: String,
+        section: String
+    },
+    selectedBattleCards: {
+        cards: {
+            cardId: String,
+            text: String,
+            cardType: String,
+            editing: Boolean
+        },
+        battleCardId: String,
+        isEmptyAddButtonState: Boolean,
+        titleValue: String,
+        section: String
+    },
+    isEmptyObjectionsState: Boolean,
+    isEmptyCompetitorsState: Boolean,
+    isEmptyProfilesState: Boolean,
+    isEmptyDiscoveriesState: Boolean
+  }
+})
+
+const BattleCards = mongoose.model('BattleCards', battleCardsSchema);
+
+module.exports = { BattleCards };
