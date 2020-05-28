41be392ef778884ddc898d193801b08e53a584be
Maxime Chemenda <maxime.chemenda@gmail.com>
2020-05-28 11:46:30 +0200

    last commit before new branch

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -37204,7 +37204,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54280" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65328" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
