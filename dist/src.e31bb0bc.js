e461c7a6292c06c383c94fc2b2d29bd560e1333d
Maxime Chemenda <maxime.chemenda@gmail.com>
2020-05-30 17:20:28 +0200

    make leftMenu look nicer

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -37283,7 +37283,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53031" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59655" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
