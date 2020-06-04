95e5dc3ebd3cd131dac29379912324c5a4744488
Maxime Chemenda <maxime.chemenda@gmail.com>
2020-06-04 20:50:31 +0200

    change parcel

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -37451,7 +37451,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54949" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58776" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
